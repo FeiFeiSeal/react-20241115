79f62b72b6046ffc1bdff989f262a685f4ef1efb
Pearlie <feifeiseal@gmail.com>
2024-12-17 00:30:17 +0800

    [fix]: 修正預設為空時找不到物件為空的問題

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,12 @@
+/*
+ * @Author:Wendy
+ * @Date:2023-09-14 14:28:26
+ * @LastEditors:Wendy
+ * @LastEditTime:2024-12-17 00:28:51
+ * @Description:
+ */
 import { useState } from 'react'
-import { CORE_CONCEPTS, EXAMPLES } from './data';
+import { CORE_CONCEPTS, EXAMPLES } from './data.js';
 import CoreConcept from './components/CoreConcept'
 import Header from './components/Header'
 import TabButton from './components/TabButton'
@@ -13,7 +20,8 @@ function App() {
     console.log(tabContent);
     
   }
-  console.log('app');
+  // console.log('app');
+  console.log(EXAMPLES);
   
   return (
     <div>
@@ -37,12 +45,11 @@ function App() {
           <TabButton onClick={() => handleClick('state')}>State</TabButton>
         </menu>
         <div id="tab-content">
-          {!tabContent? <p>Please select a topic</p> : null }
-          <h3>{EXAMPLES[tabContent].title}</h3>
-          <p>{EXAMPLES[tabContent].description}</p>
-          <pre>
-            <code>{EXAMPLES[tabContent].code}</code>
-          </pre>
+          {!tabContent? <p>Please select a topic</p> : (
+          <><h3>{EXAMPLES[tabContent].title}</h3><p>{EXAMPLES[tabContent].description}</p><pre>
+                <code>{EXAMPLES[tabContent].code}</code>
+              </pre></>
+        )}
         </div>
       </section>
       </main>
